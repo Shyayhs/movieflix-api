f2f0c7148a92321cdc6ab3f2e311d24a1247202f
Pedro Lucas <pedrolucasdemoura@gmail.com>
2025-06-12 12:06:21 -0300

    configurações ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+// import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+    {
+        "rules": {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+            "indent": [
+                "error",
+                4
+            ],
+            "linebreak-style": [
+                "error",
+                "windows"
+            ],
+            "quotes": [
+                "error",
+                "double"
+            ],
+            "semi": [
+                "error",
+                "always"
+            ]
+        },
+    },
+    // tseslint.configs.recommended
+]);
