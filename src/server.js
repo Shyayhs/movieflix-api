589965450ccdbde25589ebfa95be3a47ab9ac6bd
Pedro Lucas <pedrolucasdemoura@gmail.com>
2025-06-16 18:06:55 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
 const port = 3000;
-const app = express();
-const prisma = new PrismaClient();
-
-app.use(express.json());
-
-app.get("/movies", async (_ , res) => {
+const app = (0, express_1.default)();
+const prisma = new client_1.PrismaClient();
+app.use(express_1.default.json());
+app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
             title: "asc",
@@ -17,15 +19,13 @@ app.get("/movies", async (_ , res) => {
             languages: true
         }
     });
-    res.json(movies);   
+    res.json(movies);
 });
-
 app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-
-    try     {
+    try {
         await prisma.movie.create({
-            data:  {
+            data: {
                 title,
                 genre_id,
                 language_id,
@@ -33,14 +33,13 @@ app.post("/movies", async (req, res) => {
                 release_date: new Date(release_date)
             }
         });
-    } catch(erro){
-        console.error("Erro desconhecido ao cadastrar filme:", erro);
-        res.status(500).send({message: "Falha ao cadastrar um filme"});
     }
-
+    catch (erro) {
+        console.error("Erro desconhecido ao cadastrar filme:", erro);
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    }
     res.status(201).send();
 });
-
 app.listen(port, () => {
     console.log(`Servidor em execução na porta http://localhost:${port}`);
 });
