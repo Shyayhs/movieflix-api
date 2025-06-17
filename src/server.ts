939f2a93336d8cb134873991059d04f9761494d9
Pedro Lucas <pedrolucasdemoura@gmail.com>
2025-06-17 17:13:49 -0300

    adicionado filtro de filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -97,10 +97,35 @@ app.delete("/movies/:id", async (req, res) => {
 
     await prisma.movie.delete({ where: { id } });
   } catch (erro) {
-    return res.status(500).send({ message: "Não foi possível remover o filme" });
+    return res
+      .status(500)
+      .send({ message: "Não foi possível remover o filme" });
   }
 
   res.status(200).send();
+});
+
+app.get("/movies/:genreName", async (req, res) => {
+  try {
+    const moviesFilteredByGenreNames = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true,
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: "insensitive",
+          },
+        },
+      },
+    });
+
+    res.status(200).send(moviesFilteredByGenreNames);
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao filtrar filmes por gênero"});
+  }
 });
 
 app.listen(port, () => {
